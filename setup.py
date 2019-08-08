0028a37daa37f1584b20a815818f44f8ae73d88a
ucancallmealicia <adetelich@gmail.com>
2019-08-08 14:07:43 -0400

    docs

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='aspace_tools',
           'Operating System :: OS Independent'
           ],
 	  packages=['aspace_tools'],
-	  install_requires=['requests', 'paramiko', 'pymysql', 'sshtunnel', 'pandas', 'bs4'],
+	  install_requires=['requests', 'paramiko', 'pymysql', 'sshtunnel', 'pandas', 'bs4', 'utilities @ https://github.com/ucancallmealicia/utilities/tarball/master'],
 	  include_package_data=True,
 	  zip_safe=False)
 

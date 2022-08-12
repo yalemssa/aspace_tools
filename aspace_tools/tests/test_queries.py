f26de6f6d9ff7a41b51370667ffd1e8a9d935c81
ucancallmealicia <alicia.detelich@yale.edu>
2022-08-12 16:27:53 -0400

    restructure for docs

--- aspace_tools/tests/test_queries.py
+++
@@ -1,1 +0,0 @@
-#TESTS: Check if all files open, if they correspond to AS DB schema, and if the number and titles of functions correspond with what's in the directory
--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ import sphinx_rtd_theme
 #sys.path.insert(0, os.path.abspath('../../..'))
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+print(os.path.abspath('../src'))
 # sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('..'))
 
